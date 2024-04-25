bf2e6afd40ce5e200407be4df78062512e3ed5ef
Sebastian Olivares <olivares.heredia@gmail.com>
2024-04-25 12:38:37 -0600

    created and progress on Moviebook class

---
+++ movies_book.rb
@@ -0,0 +1,34 @@
+class Moviebook
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @movies = []
+    load_csv
+  end
+
+  def all
+    @movies
+  end
+
+  def create(movie)
+    @movies << movie
+    save_csv
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file_path, headers: true) do |row|
+      new_movie = Movie.new(row["title"], row["director"], row["year"], row["score"])
+      create(new_movie)
+    end
+  end
+
+  def save_csv
+    CSV.open(@csv_file_path, "wb") do |csv|
+      csv << ["title", "director", "year", "score"]
+      @movies.each do |movie|
+        csv << [movie.title, movie.director, movie.year, movie.score]
+      end
+    end
+  end
+end
